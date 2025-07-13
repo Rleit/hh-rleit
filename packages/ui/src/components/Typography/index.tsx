d90b2b6ab09984e510bf98d7c96db685d6ccb11f
Rainer Leit <rainerleit@gmail.com>
2025-07-13 18:34:22 +0300

    minor changes: font to poppins

--- packages/ui/src/components/Typography/index.tsx
+++ packages/ui/src/components/Typography/index.tsx
@@ -1,10 +1,10 @@
-import * as React from "react";
+import * as React from 'react';
 
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const headingDefaults = `
 	color: #001147;
-	font-family: "Montserrat";
+	font-family: "Poppins";
 	font-weight: 800;
 `;
 
@@ -13,8 +13,8 @@ export const HeadingOne = styled.h1`
   font-size: 60px;
   line-height: 100px;
   letter-spacing: 2.15px;
-	margin: 0;
-	overflow-wrap: break-word;
+  margin: 0;
+  overflow-wrap: break-word;
 `;
 
 export const HeadingTwo = styled.h2`
@@ -25,18 +25,18 @@ export const HeadingTwo = styled.h2`
 `;
 
 export const HeadingThree = styled.h3`
-	font-family: "Montserrat";
+  font-family: 'Poppins';
   font-weight: 500;
-	color: #fff;
-	font-size: 22px;
-	line-height: 25px;
-	letter-spacing: 1.15px;
-`
+  color: #fff;
+  font-size: 22px;
+  line-height: 25px;
+  letter-spacing: 1.15px;
+`;
 
 export const Paragraph = styled.p`
   color: #191919;
   font-weight: 500;
   letter-spacing: 0.65px;
   line-height: 32px;
-  font-family: "Montserrat";
+  font-family: 'Poppins';
 `;
